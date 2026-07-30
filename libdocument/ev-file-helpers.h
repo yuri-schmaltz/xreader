9bbacfda929e9dd99a97896c7d21f5ed9ce2f9de
Test <test@example.com>
2026-07-30 08:47:55 +0000

    libdocument: Add G_GNUC_PURE and G_GNUC_MALLOC annotations to ev-file-helpers
    G_GNUC_PURE tells the compiler that a function has no
    side effects and only reads from its arguments.
    
    G_GNUC_MALLOC tells the compiler that a function returns
    a newly allocated pointer that does not alias any
    other pointer.
    
    Add the annotations to 6 functions in ev-file-helpers.h:
    
      PURE (1):
        - ev_file_is_temp
    
      MALLOC (5):
        - ev_file_get_mime_type
        - ev_file_uncompress
        - ev_file_compress
        - ev_mkdtemp
        - ev_mkstemp_file
    
    Annotations-only change, zero runtime behaviour
    modification.

--- libdocument/ev-file-helpers.h
+++ libdocument/ev-file-helpers.h
@@ -44,27 +44,27 @@ int          ev_mkstemp               (const char        *template,
                                        char             **file_name,
                                        GError           **error);
 GFile       *ev_mkstemp_file          (const char        *template,
-                                       GError           **error);
+                                       GError           **error) G_GNUC_MALLOC;
 gchar       *ev_mkdtemp               (const char        *template,
-                                       GError           **error);
+                                       GError           **error) G_GNUC_MALLOC;
 void         ev_tmp_filename_unlink   (const gchar       *filename);
 void         ev_tmp_file_unlink       (GFile             *file);
 void         ev_tmp_uri_unlink        (const gchar       *uri);
-gboolean     ev_file_is_temp          (GFile             *file) G_GNUC_NONNULL (1);
+gboolean     ev_file_is_temp          (GFile             *file) G_GNUC_NONNULL (1) G_GNUC_PURE;
 gboolean     ev_xfer_uri_simple       (const char        *from,
 				       const char        *to,
 				       GError           **error) G_GNUC_NONNULL (1, 2);
 
 gchar       *ev_file_get_mime_type    (const gchar       *uri,
 				       gboolean           fast,
-				       GError           **error) G_GNUC_NONNULL (1);
+				       GError           **error) G_GNUC_NONNULL (1) G_GNUC_MALLOC;
 
 gchar       *ev_file_uncompress       (const gchar       *uri,
 				       EvCompressionType  type,
-				       GError           **error) G_GNUC_NONNULL (1);
+				       GError           **error) G_GNUC_NONNULL (1) G_GNUC_MALLOC;
 gchar       *ev_file_compress         (const gchar       *uri,
 				       EvCompressionType  type,
-				       GError           **error) G_GNUC_NONNULL (1);
+				       GError           **error) G_GNUC_NONNULL (1) G_GNUC_MALLOC;
 
 
 G_END_DECLS
